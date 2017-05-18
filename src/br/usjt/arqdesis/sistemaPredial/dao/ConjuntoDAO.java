aff0132283a4d68831c9152cac131061ef2b71ea
HugoYoshimura <HugoYoshimura@users.noreply.github.com>
2017-05-18 16:15:22 -0300

    Update ConjuntoDAO.java

--- src/br/usjt/arqdesis/sistemaPredial/dao/ConjuntoDAO.java
+++ src/br/usjt/arqdesis/sistemaPredial/dao/ConjuntoDAO.java
@@ -13,6 +13,10 @@ import com.sun.org.apache.regexp.internal.RE;
 import br.usjt.arqdesis.sistemaPredial.model.Conjunto;
 
 public class ConjuntoDAO {
+	/**
+	 * Alterar conjunto com os dados da empresa
+	 * @param conjunto objeto conjunto com o número do conjunto
+	 */
 	public void criar(Conjunto conjunto) {
 		String sqlUpdate = "UPDATE conjunto SET status = ?, id_empresa = ? WHERE numeroConjunto = ?";
 		try (Connection conn = ConnectionFactory.obtemConexao();
@@ -25,6 +29,10 @@ public class ConjuntoDAO {
 		}
 	}
 	
+	/**
+	 * Alterar conjunto retirando os dados da empresa
+	 * @param conjunto objeto conjunto com o número do conjunto
+	 */
 	public void excluir(Conjunto conjunto) {
 		String sqlUpdate = "UPDATE conjunto SET status = ?, id_empresa = ? WHERE numeroConjunto = ?";
 		try (Connection conn = ConnectionFactory.obtemConexao();
@@ -38,121 +46,12 @@ public class ConjuntoDAO {
 		}
 	}
 
-	public List<Conjunto> carregarConjuntosVagos() {
-		Conjunto conjunto;
-		
-		List<Conjunto> lista = new ArrayList<Conjunto>();
-		
-		String sqlSelect = "SELECT * from conjunto WHERE status = 0 ORDER BY numeroConjunto";
-		
-		try (Connection conn = ConnectionFactory.obtemConexao();
-				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
-			try (ResultSet rs = stm.executeQuery();) {
-				while(rs.next()) {
-					conjunto = new Conjunto();
-					conjunto.setIdConjunto(rs.getInt("id_conjunto"));
-					conjunto.setNumeroConjunto(rs.getInt("numeroConjunto"));
-					conjunto.setIdEmpresa(rs.getInt("id_empresa"));
-					if(rs.getBoolean("status")) {
-						conjunto.setStatus(true);
-					} else {
-						conjunto.setStatus(false);
-					}
-					
-					lista.add(conjunto);
-				}
-				conn.close();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return lista;
-	}
-	
-	public List<Conjunto> carregarConjuntosEmpresa(int idEmpresa) {
-		Conjunto conjunto;
-		
-		List<Conjunto> lista = new ArrayList<Conjunto>();
-		
-		String sqlSelect = "SELECT * FROM conjunto WHERE id_empresa = ? ORDER BY numeroConjunto";
-		
-		try (Connection conn = ConnectionFactory.obtemConexao();
-				PreparedStatement stm = conn.prepareStatement(sqlSelect); ) {
-			try(ResultSet rs = stm.executeQuery()) {
-				while(rs.next()) {
-					conjunto = new Conjunto();
-					conjunto.setIdConjunto(rs.getInt("id_conjunto"));
-					conjunto.setNumeroConjunto(rs.getInt("numeroConjunto"));
-					conjunto.setStatus(true);
-					conjunto.setIdEmpresa(rs.getInt("id_empresa"));
-					
-					lista.add(conjunto);
-				}
-				conn.close();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return lista;
-	}
-	
-	public void insert(Conjunto conjunto) {
-		String sqlInsert = "INSERT INTO conjunto(numeroConjunto, status) VALUES (?, ?)";
-		try (Connection conn = ConnectionFactory.obtemConexao();
-				PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
-			stm.setInt(1, conjunto.getNumeroConjunto());
-			if(conjunto.isStatus()) {
-				stm.setInt(2, 1);
-			} else {
-				stm.setInt(2, 0);
-			}
-			stm.setInt(3, conjunto.getIdEmpresa());
-			stm.execute();
-			String sqlQuery = "SELECT LAST_INSERT_ID()";
-			try (PreparedStatement stm2 = conn.prepareStatement(sqlQuery); ResultSet rs = stm2.executeQuery();) {
-				if (rs.next()) {
-					conjunto.setIdConjunto(rs.getInt(1));
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void update(Conjunto conjunto) {
-		String sqlUpdate = "UPDATE conjunto SET numeroConjunto = ?, status = ?, id_empresa = ? WHERE id_conjunto = ?";
-		// usando o try with resources do Java 7, que fecha o que abriu
-		try (Connection conn = ConnectionFactory.obtemConexao();
-				PreparedStatement stm = conn.prepareStatement(sqlUpdate);) {
-			stm.setInt(1, conjunto.getNumeroConjunto());
-			if(conjunto.isStatus()){
-				stm.setInt(2, 1);
-			} else {
-				stm.setInt(2, 0);
-			}
-			stm.setInt(3, conjunto.getIdEmpresa());
-			stm.setInt(4, conjunto.getIdConjunto());
-			stm.execute();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void delete(int numeroConjunto) {
-		String sqlDelete = "DELETE FROM conjunto WHERE numeroConjunto = ?";
-		// usando o try with resources do Java 7, que fecha o que abriu
-		try (Connection conn = ConnectionFactory.obtemConexao();
-				PreparedStatement stm = conn.prepareStatement(sqlDelete);) {
-			stm.setInt(1, numeroConjunto);
-			stm.execute();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public Conjunto select(int numeroConjunto) {
+	/**
+	 * Consultar conjunto a partir do número do conjunto
+	 * @param numeroConjunto Número do conjunto a ser consultado
+	 * @return Objeto conjunto com os dados do conjunto consultado
+	 */
+	public Conjunto carregar(int numeroConjunto) {
 		Conjunto conjunto = new Conjunto();
 		String sqlSelect = "SELECT id_conjunto, status, id_empresa FROM conjunto WHERE conjunto.numeroConjunto = ?";
 		// usando o try with resources do Java 7, que fecha o que abriu
@@ -185,21 +84,26 @@ public class ConjuntoDAO {
 		return conjunto;
 	}
 	
-	public List<Conjunto> selectAll() {
+	/**
+	 * Carrega todos os conjuntos vagos
+	 * @return Lista de conjuntos com todos o conjuntos vagos
+	 */
+	public List<Conjunto> carregarConjuntosVagos() {
 		Conjunto conjunto;
 		
 		List<Conjunto> lista = new ArrayList<Conjunto>();
 		
-		String sqlSelect = "SELECT * FROM conjunto WHERE id_empresa = ? ORDER BY numeroConjunto";
+		String sqlSelect = "SELECT * from conjunto WHERE status = 0 ORDER BY numeroConjunto";
 		
-		try(Connection conn = ConnectionFactory.obtemConexao(); PreparedStatement stm = conn.prepareStatement(sqlSelect); ) {
-			try(ResultSet rs = stm.executeQuery();) {
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			try (ResultSet rs = stm.executeQuery();) {
 				while(rs.next()) {
 					conjunto = new Conjunto();
 					conjunto.setIdConjunto(rs.getInt("id_conjunto"));
 					conjunto.setNumeroConjunto(rs.getInt("numeroConjunto"));
 					conjunto.setIdEmpresa(rs.getInt("id_empresa"));
-					if(rs.getInt("status") == 1) {
+					if(rs.getBoolean("status")) {
 						conjunto.setStatus(true);
 					} else {
 						conjunto.setStatus(false);
@@ -209,9 +113,142 @@ public class ConjuntoDAO {
 				}
 				conn.close();
 			}
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return lista;
 	}
+	
+	/**
+	 * Carregar todos os conjuntos utilizados por uma empresa 
+	 * @param idEmpresa id da empresa
+	 * @return Lista de conjuntos utilizados pela empresa do id informado 
+	 */
+	public List<Conjunto> carregarConjuntosEmpresa(int idEmpresa) {
+		Conjunto conjunto;
+		
+		List<Conjunto> lista = new ArrayList<Conjunto>();
+		
+		String sqlSelect = "SELECT * FROM conjunto WHERE id_empresa = ? ORDER BY numeroConjunto";
+		
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlSelect); ) {
+			try(ResultSet rs = stm.executeQuery()) {
+				while(rs.next()) {
+					conjunto = new Conjunto();
+					conjunto.setIdConjunto(rs.getInt("id_conjunto"));
+					conjunto.setNumeroConjunto(rs.getInt("numeroConjunto"));
+					conjunto.setStatus(true);
+					conjunto.setIdEmpresa(rs.getInt("id_empresa"));
+					
+					lista.add(conjunto);
+				}
+				conn.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return lista;
+	}
+	
+	/**
+	 * Carregar todos os conjuntos utilizados por uma empresa 
+	 * @param idEmpresa id da empresa
+	 * @return Lista de conjuntos utilizados pela empresa do id informado 
+	 */
+	public List<Conjunto> carregarConjuntosEmpresaEVagos(int idEmpresa) {
+		Conjunto conjunto;
+		
+		List<Conjunto> lista = new ArrayList<Conjunto>();
+		
+		String sqlSelect = "SELECT * FROM conjunto WHERE id_empresa = ? OR id_empresa = null ORDER BY numeroConjunto";
+		
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlSelect); ) {
+			try(ResultSet rs = stm.executeQuery()) {
+				while(rs.next()) {
+					conjunto = new Conjunto();
+					conjunto.setIdConjunto(rs.getInt("id_conjunto"));
+					conjunto.setNumeroConjunto(rs.getInt("numeroConjunto"));
+					conjunto.setStatus(true);
+					conjunto.setIdEmpresa(rs.getInt("id_empresa"));
+					
+					lista.add(conjunto);
+				}
+				conn.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return lista;
+	}
+	
+	/**
+	 * Criar um conjunto novo
+	 * @param conjunto conjunto com dados do conjunto novo
+	 */
+	public void insert(Conjunto conjunto) {
+		String sqlInsert = "INSERT INTO conjunto(numeroConjunto, status) VALUES (?, ?)";
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
+			stm.setInt(1, conjunto.getNumeroConjunto());
+			if(conjunto.isStatus()) {
+				stm.setInt(2, 1);
+			} else {
+				stm.setInt(2, 0);
+			}
+			stm.setInt(3, conjunto.getIdEmpresa());
+			stm.execute();
+			String sqlQuery = "SELECT LAST_INSERT_ID()";
+			try (PreparedStatement stm2 = conn.prepareStatement(sqlQuery); ResultSet rs = stm2.executeQuery();) {
+				if (rs.next()) {
+					conjunto.setIdConjunto(rs.getInt(1));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Atualiza os dados do conjunto
+	 * @param conjunto Objeto com dados a serem atualizados
+	 */
+	public void update(Conjunto conjunto) {
+		String sqlUpdate = "UPDATE conjunto SET numeroConjunto = ?, status = ?, id_empresa = ? WHERE id_conjunto = ?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlUpdate);) {
+			stm.setInt(1, conjunto.getNumeroConjunto());
+			if(conjunto.isStatus()){
+				stm.setInt(2, 1);
+			} else {
+				stm.setInt(2, 0);
+			}
+			stm.setInt(3, conjunto.getIdEmpresa());
+			stm.setInt(4, conjunto.getIdConjunto());
+			stm.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Exclui um conjunto
+	 * @param numeroConjunto Número do conjunto a ser excluído
+	 */
+	public void delete(int numeroConjunto) {
+		String sqlDelete = "DELETE FROM conjunto WHERE numeroConjunto = ?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlDelete);) {
+			stm.setInt(1, numeroConjunto);
+			stm.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
